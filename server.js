ab7ea817f1845b11007bf4a01fcf7e5426edddf4
bsskalyan <31906445+bsskalyan@users.noreply.github.com>
2025-09-22 22:29:20 +0200

    Update dummy data on  server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import axios from "axios";
+import cors from "cors"; 
 import * as cheerio from "cheerio";
 
 const app = express();
+app.use(cors()); 
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/search", async (req, res) => {
